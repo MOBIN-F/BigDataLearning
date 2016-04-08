247d49ed0575586934b3f2134c59b67f669bdf2e
Mobin <996833461@qq.com>
2016-04-08 09:27:16 -0700

    union intersection

---
+++ src/main/scala/com/mobin/Spark/Union.scala
@@ -0,0 +1,18 @@
+package com.mobin.Spark
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by hadoop on 4/8/16.
+  */
+object Union {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local").setAppName("map")
+    val sc = new SparkContext(conf)
+    val rdd1 = sc.parallelize(1 to 4)
+    val rdd2 = sc.parallelize(3 to 5)
+    val unionRDD = rdd1.intersection(rdd2) //union  intersection
+    unionRDD.collect.foreach(x => print(x + " "))
+    sc.stop()
+  }
+}
