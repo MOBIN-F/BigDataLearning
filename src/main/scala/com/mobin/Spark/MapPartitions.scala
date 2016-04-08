f0f6ea5219037ed4faf48c3d06cc0243a955f020
Mobin <996833461@qq.com>
2016-04-07 23:43:07 -0700

    mapPartitions   mapPartitionsWithIndex

--- src/main/scala/com/mobin/Spark/MapPartitions.scala
+++ src/main/scala/com/mobin/Spark/MapPartitions.scala
@@ -4,15 +4,16 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 /**
   * Created by hadoop on 4/7/16.
+  * mapPartitionsWithIndex:将[mapPartitionsWithIndex]注释部分去掉即是
   */
 object MapPartitions {
  // 输出有女性的名字：
-  def partitionsFun(iter : Iterator[(String,String)]) : Iterator[String] = {
+  def partitionsFun(/*[mapPartitionsWithIndex]  index : Int,*/iter : Iterator[(String,String)]) : Iterator[String] = {
     var woman = List[String]()
     while (iter.hasNext){
       val next = iter.next()
       next match {
-        case (_,"female") => woman = next._1 :: woman
+        case (_,"female") => woman = /*[mapPartitionsWithIndex]"["+index+"]"+*/next._1 :: woman
         //case (_,"female") => woman =  next._1.toList .:: (woman)  错误写法
         case _ =>
       }
@@ -24,11 +25,11 @@ object MapPartitions {
   def main(args: Array[String]) {
     val conf = new SparkConf().setMaster("local").setAppName("flatmap")
     val sc = new SparkContext(conf)
-    val l = List(("kpop","female"),("lucy","female"),("mobin","male"))
+    val l = List(("kpop","female"),("zorro","male"),("mobin","male"),("lucy","female"))
     val rdd = sc.parallelize(l,2)
    // val mp = rdd.mapPartitions(x => x.filter(_._2 == "female")).map(x => x._1)
-    val mp = rdd.mapPartitions(partitionsFun,true)
-    println(mp.count)
+    val mp = rdd.mapPartitions(partitionsFun)
+    //[mapPartitionsWithIndex]  val mp = rdd.mapPartitionsWithIndex(partitionsFun)
     mp.collect.foreach(x => (print(x +" "))) //将分区中的元素转换成Aarray再输出
   }
 }
