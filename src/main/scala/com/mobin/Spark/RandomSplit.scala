a9d6aae9515f58e824d8e47c681ae62ef5e3e1fd
Mobin <996833461@qq.com>
2016-04-09 11:05:26 -0700

    randomSplit

--- src/main/scala/com/mobin/Spark/RandomSplit.scala
+++ src/main/scala/com/mobin/Spark/RandomSplit.scala
@@ -12,9 +12,9 @@ object RandomSplit {
     val sc = new SparkContext(conf)
     val rdd = sc.parallelize(1 to 10)
     val randomSplitRDD = rdd.randomSplit(Array(1.0,2.0,7.0))
-    randomSplitRDD(0).foreach(x => print(x +" "))
-    randomSplitRDD(1).foreach(x => print(x +" "))
-    randomSplitRDD(2).foreach(x => print(x +" "))
+    randomSplitRDD(0).foreach(x => print(x +" gg"))
+    randomSplitRDD(1).foreach(x => print(x +" rr"))
+    randomSplitRDD(2).foreach(x => print(x +" tt"))
     sc.stop
   }
 }
