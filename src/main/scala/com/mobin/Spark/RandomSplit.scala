6dabb7e20c479f5fddc2448edf58bddf1ef88ef2
Mobin <996833461@qq.com>
2016-04-09 09:57:12 -0700

    randomSplit

---
+++ src/main/scala/com/mobin/Spark/RandomSplit.scala
@@ -0,0 +1,20 @@
+package com.mobin.Spark
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by hadoop on 4/9/16.
+  */
+object RandomSplit {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local").setAppName("map")
+    val sc = new SparkContext(conf)
+    val rdd = sc.parallelize(1 to 10)
+    val randomSplitRDD = rdd.randomSplit(Array(1.0,2.0,7.0))
+    randomSplitRDD(0).foreach(x => print(x +" "))
+    randomSplitRDD(1).foreach(x => print(x +" "))
+    randomSplitRDD(2).foreach(x => print(x +" "))
+    sc.stop
+  }
+}
