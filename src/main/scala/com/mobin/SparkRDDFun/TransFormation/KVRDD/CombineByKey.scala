c1610229da21ece28ab5c7c7954341f03d051e2a
Mobin <996833461@qq.com>
2016-04-11 08:52:49 -0700

    CombineByKey

--- src/main/scala/com/mobin/SparkRDDFun/TransFormation/KVRDD/CombineByKey.scala
+++ src/main/scala/com/mobin/SparkRDDFun/TransFormation/KVRDD/CombineByKey.scala
@@ -22,6 +22,7 @@ object CombineByKey {
       (peo: (String, Int), x: String) => (peo._1+","+x, peo._2 + 1),
       (sex1: (String, Int), sex2: (String, Int)) => (sex1._1 +","+ sex2._1, sex1._2 + sex2._2))
 
+
     combinByKeyRDD.foreach(println)
     sc.stop()
   }
