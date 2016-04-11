f7f4166aafd039d86c9f48fc0979601076e5ed05
Mobin <996833461@qq.com>
2016-04-11 08:52:20 -0700

    Spark常用函数

--- src/main/scala/com/mobin/SparkRDDFun/TransFormation/KVRDD/CombineByKey.scala
+++ src/main/scala/com/mobin/SparkRDDFun/TransFormation/KVRDD/CombineByKey.scala
@@ -17,13 +17,13 @@ object CombineByKey {
     val sc = new SparkContext(conf)
     val people = List(("male", "Mobin"), ("male", "Kpop"), ("female", "Lucy"), ("male", "Lufei"), ("female", "Amy"))
     val rdd = sc.parallelize(people)
-
     val combinByKeyRDD = rdd.combineByKey(
       (x: String) => (x, 1),
       (peo: (String, Int), x: String) => (peo._1+","+x, peo._2 + 1),
       (sex1: (String, Int), sex2: (String, Int)) => (sex1._1 +","+ sex2._1, sex1._2 + sex2._2))
 
-     combinByKeyRDD.foreach(println)
+    combinByKeyRDD.foreach(println)
+    sc.stop()
   }
 
 
