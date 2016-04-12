4a85c06a4889173d171f4c25b6a4c6b30cafd649
Mobin <996833461@qq.com>
2016-04-11 20:44:52 -0700

    combineByKey

--- src/main/scala/com/mobin/SparkRDDFun/TransFormation/KVRDD/CombineByKey.scala
+++ src/main/scala/com/mobin/SparkRDDFun/TransFormation/KVRDD/CombineByKey.scala
@@ -4,7 +4,7 @@ import org.apache.spark.{HashPartitioner, SparkContext, SparkConf}
 
 /**
   * Created by hadoop on 4/11/16.
-  *
+  *统计男性和女生的个数，并以（性别，（名字，名字....），个数）的形式输出
   */
 object CombineByKey {
 
@@ -18,16 +18,12 @@ object CombineByKey {
     val people = List(("male", "Mobin"), ("male", "Kpop"), ("female", "Lucy"), ("male", "Lufei"), ("female", "Amy"))
     val rdd = sc.parallelize(people)
     val combinByKeyRDD = rdd.combineByKey(
-      (x: String) => (x, 1),
-      (peo: (String, Int), x: String) => (peo._1+","+x, peo._2 + 1),
-      (sex1: (String, Int), sex2: (String, Int)) => (sex1._1 +","+ sex2._1, sex1._2 + sex2._2))
-
+      (x: String) => (List(x), 1),
+      (peo: (List[String], Int), x : String) => (x :: peo._1, peo._2 + 1),
+      (sex1: (List[String], Int), sex2: (List[String], Int)) => (sex1._1 ::: sex2._1, sex1._2 + sex2._2))
 
     combinByKeyRDD.foreach(println)
+    println(combinByKeyRDD.toDebugString)
     sc.stop()
   }
-
-
-
-
 }
