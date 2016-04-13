d17aef19417762d21831cbe83469536e8447ea45
Mobin <996833461@qq.com>
2016-04-13 05:54:17 -0700

    HDFS

--- src/main/scala/com/mobin/HDFS/WriteToHDFS.scala
+++ src/main/scala/com/mobin/HDFS/WriteToHDFS.scala
@@ -9,14 +9,12 @@ import scala.util.Random
   * 为文件中的第一个字段拼接一个随机值再写入HDFS
   */
 object WriteToHDFS {
-
   def main(args: Array[String]) {
     if (args.length < 2) {
       System.err.println("Usage: WriteToHDFS <InputPath> <OutputPath>\n")
       System.exit(1)
     }
-
-    val conf = new SparkConf().setAppName("sogou")
+    val conf = new SparkConf().setAppName("WriteToHDFS")
     val sc = new SparkContext(conf)
     val sgfile = sc.textFile(args(0))
 
@@ -26,10 +24,8 @@ object WriteToHDFS {
         val one = line(0) +"-"+ new Random().nextInt()
         (one, line(1),line(2),line(3),line(4),line(5))
       }
-
     })
     rdd.saveAsTextFile(args(1))
     sc.stop()
-
   }
 }
