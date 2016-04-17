bb4527fe365adb63a73286304e101322e2c5b207
Mobin <996833461@qq.com>
2016-04-17 05:05:18 -0700

    HDFS

--- src/main/scala/com/mobin/HDFS/WriteToHDFS.scala
+++ src/main/scala/com/mobin/HDFS/WriteToHDFS.scala
@@ -14,17 +14,17 @@ object WriteToHDFS {
       System.err.println("Usage: WriteToHDFS <InputPath> <OutputPath>\n")
       System.exit(1)
     }
-    val conf = new SparkConf().setAppName("WriteToHDFS")
+    val conf = new SparkConf().setMaster("local").setAppName("WriteToHDFS")
     val sc = new SparkContext(conf)
     val sgfile = sc.textFile(args(0))
 
     val rdd = sgfile.map(lines => {
-      val line = lines.split("\t")
+      val line = lines.split("\\s")
       if(line.length == 6){
         val one = line(0) +"-"+ new Random().nextInt()
-        val lines = Array(one, line(1),line(2),line(3),line(4),line(5))
-        lines.mkString(",")
-      }
+        one+","+line(1)+","+line(2).getBytes+","+line(3)+","+line(4)+","+line(5)
+      }else   //如果这样写  一定不能只写if语句  还要加上else语句，否则没有通过if的，将被视了() 否则后期通过Phoenix导入到HBase中会因为字段不合法而报错
+        "mobin1"+","+"mobin2"+","+"mobin3"+"mobin4"+","+"mobin5"+","+"mobin6"
     })
     rdd.saveAsTextFile(args(1))
     sc.stop()
