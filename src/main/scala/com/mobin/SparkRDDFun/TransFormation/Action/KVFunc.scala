8c3ef3878aa839067ef609bf66f3b8145ecd5ee4
Mobin <996833461@qq.com>
2016-04-20 00:27:03 -0700

    countByKey  collectAsMap  lookup

---
+++ src/main/scala/com/mobin/SparkRDDFun/TransFormation/Action/KVFunc.scala
@@ -0,0 +1,26 @@
+package com.mobin.SparkRDDFun.TransFormation.Action
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by hadoop on 4/19/16.
+  */
+object KVFunc {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local").setAppName("KVFunc")
+    val sc = new SparkContext(conf)
+    val arr = List(("A", 1), ("B", 2), ("A", 2), ("B", 3))
+    val rdd = sc.parallelize(arr,2)
+    val countByKeyRDD = rdd.countByKey()
+    val collectAsMapRDD = rdd.collectAsMap()
+    val lookupRDD = rdd.lookup("A")
+    println("countByKey:")
+    countByKeyRDD.foreach(print)
+    println("\ncollectAsMap:")
+    collectAsMapRDD.foreach(print)
+    println("\nlookup:")
+    lookupRDD.foreach(x => print(x))
+    sc.stop
+  }
+}
